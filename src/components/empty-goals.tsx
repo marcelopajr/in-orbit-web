cdd939ea53a548b3031067f33c9212a7c76c37dc
Marcelo Pereira <marcelo96jr@gmail.com>
2024-09-12 17:57:16 -0300

    chore: add react query config

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,10 +1,10 @@
 import { Plus } from 'lucide-react';
-import { Button } from '../components/ui/button';
-import { DialogTrigger } from '../components/ui/dialog';
+import { Button } from './ui/button';
+import { DialogTrigger } from './ui/dialog';
 import logo from '../assets/logo-in-orbit.svg';
 import letsStart from '../assets/lets-start-illustration.svg';
 
-export function EmptyGoal() {
+export function EmptyGoals() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-8">
       <img src={logo} alt="In.orbit logo" />
